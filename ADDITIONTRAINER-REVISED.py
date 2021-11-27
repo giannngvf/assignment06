72fb2c9b90e67a2d09e2ed13c7e872210d1a809c
Gian Gregorio V. Florido <gianflorido1414@gmail.com>
2021-11-27 13:56:18 -0800

    I renamed my variables so that it will be much more specific what that variable is for.

---
+++ ADDITIONTRAINER-REVISED.py
@@ -0,0 +1,21 @@
+# Program 2: Addition Trainer
+# Create a program that automatically generate two random numbers to add (0 to 99)
+# Let the user answer and evaluate if the user has the correct answer
+# The program will ask the user 10 addition operation
+# Display the result summary of the 10 operations (ex 9/10)
+print("Welcome to Addition Trainer!")
+print("I will ask you 10 addition problems.")
+import random
+score = 0
+for i in range(10):
+	first_random_num = random.randint(0, 99)
+	second_random_num = random.randint(0, 99)
+	print("What is the sum of",first_random_num, "and",second_random_num,"?")
+	user_ans = int(input("Enter you answer: "))
+	ans = first_random_num + second_random_num
+	if user_ans == ans:
+		print("That's right!")
+		score = score + 1
+	else:
+		print("Wrong!")
+print("You got",score,"/ 10!")
